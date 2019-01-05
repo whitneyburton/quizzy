d5334c2c3affc1541876ab79b6b4230ae27543ea
Whitney Burton <whitneyburton9@gmail.com>
2019-01-04 20:46:41 -0700

    Create FlashcardContainer

---
+++ src/components/FlashcardContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Flashcard from './Flashcard'
+import '../styles/main.scss';
+
+export default function FlashcardContainer() {
+    return (
+    <div className="FlashcardContainer">
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+        <Flashcard />
+    </div>
+  );
+}
---
+++ src/tests/FlashcardContainer.test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import FlashcardContainer from '../Components/FlashcardContainer';
+import { shallow } from 'enzyme';
