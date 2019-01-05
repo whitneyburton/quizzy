b4e43de52b6214f904206c0a25a92d9fa7c01a49
Whitney Burton <whitneyburton9@gmail.com>
2019-01-04 20:49:45 -0700

    Add basic html structure to Flashcard

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -11,9 +11,18 @@ export default class Flashcard extends Component {
   render() {
     // let { flashcards } = this.state.props;
      return (
-      <div className="Flashcard">
-        
-        <p className="c">Flashcard</p>
+       <div className="Flashcard">
+        <p className="question-counter">Question 1/15</p>         
+         <p>Question Here</p>
+         <button
+            className="answer-one buttons"
+           type="button">Answer One</button>
+          <button
+            className="answer-two buttons"
+           type="button">Answer Two</button>
+          <button
+            className="answer-three buttons"
+            type="button">Answer Three</button>
       </div>
     );
   }
