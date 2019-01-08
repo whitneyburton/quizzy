78c302163db988e667045b6186de21d773329f8a
Whitney Burton <whitneyburton9@gmail.com>
2019-01-07 22:53:29 -0700

    Add dynamic buttons to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ export default class NavBar extends Component {
   constructor() {
     super();
     this.state = {
+      categories: ['Mutator', 'Accessor', 'Iteration', 'All Methods!']
     }
   }
 
@@ -14,27 +15,23 @@ export default class NavBar extends Component {
   }
 
   render() {
+    let { categories } = this.state;
     return (
       <div className="NavBar">
         <h1 className="quizzy-title">QUIZZY!</h1>
         <div className="filter-category-buttons">
           <p className="choose-category">Choose an array prototype category:</p>
-          <button
-            onClick={this.passCategory}
-            className="mutator-button buttons filters"
-            type="button">Mutator</button>
-          <button
-            onClick={this.passCategory}            
-            className="accessor-button buttons filters"
-            type="button">Accessor</button>
-          <button
-            onClick={this.passCategory}
-            className="iteration-button buttons filters"
-            type="button">Iteration</button>
-          <button
-            onClick={this.passCategory}
-            className="all-methods-button buttons filters"
-            type="button">All Methods!</button>
+          {categories.map(category => {
+            return (
+              <button
+                onClick={this.passCategory}
+                className="buttons filters"
+                type="button">{category}
+              </button>
+            )
+          })
+          }
+
         </div>
       </div>
     );
