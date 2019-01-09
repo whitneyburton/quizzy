bb8c89806166e50e40db4ebfbd3f6b37e494877d
Whitney Burton <whitneyburton9@gmail.com>
2019-01-08 17:31:05 -0700

    Add all App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -9,7 +9,7 @@ describe('App', () => {
 
   beforeEach(() => {
     localStorage.setItem('incorrectFlashcardsStorage', '[1, 2, 3]');
-    wrapper = shallow(
+    wrapper = mount(
       <App />
     )
   })
@@ -24,7 +24,7 @@ describe('App', () => {
     ReactDOM.unmountComponentAtNode(div);
   })
 
-  it('initially should have the appropriate default state', () => {
+  it('should have the appropriate default state', () => {
     expect(wrapper.state()).toEqual({
       category: null,
       error: null,
@@ -35,7 +35,41 @@ describe('App', () => {
     expect(wrapper.state().incorrectFlashcards).toEqual(expect.arrayContaining([]))
   })
 
-  it('should remove localStorage key and reset state when deleteAllStorage is called', () => {
+  it('should render the NavBar, PlayerControl, and FlashcardContainer components', () => {
+    wrapper.setState({ error: false })
+    expect(wrapper.find('.NavBar').length).toEqual(1)
+    expect(wrapper.find('.PlayerControl').length).toEqual(1)
+    expect(wrapper.find('.FlashcardContainer').length).toEqual(1)
+  })
+
+  it('should update local storage', () => {
+    let incorrectFlashcards = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    wrapper.setState({ incorrectFlashcards });
+    expect(wrapper.state().incorrectFlashcards.length).toEqual(3);
+    let newIncorrectFlashcards = { id: 4 };
+    wrapper.instance().saveToStorage(newIncorrectFlashcards);
+    let itemsInStorage = JSON.parse(localStorage.getItem('incorrectFlashcardsStorage')).length;
+    expect(itemsInStorage).toEqual(4);
+  });
+
+  it('should retrieve the incorrect flashcards from storage and set them to state', () => {
+    wrapper.setState({
+      flashcards: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+    });
+    let incorrectFlashcards = wrapper.instance().retrieveFromStorage();
+    expect(incorrectFlashcards).toEqual([{ id: 1 }, { id: 2 }, { id: 3 }]);
+  })
+
+  it('should remove correctly answered flashcards from storage', () => {
+    let flashcard = { id: 2 }
+    let itemsInStorage = JSON.parse(localStorage.getItem('incorrectFlashcardsStorage'));
+    expect(itemsInStorage).toEqual([1, 2, 3]);
+    wrapper.instance().removeCorrectFromStorage(flashcard);
+    itemsInStorage = JSON.parse(localStorage.getItem('incorrectFlashcardsStorage'));
+    expect(itemsInStorage).toEqual([1, 3]);
+  })
+
+  it('should remove the local storage key and reset state when deleteAllStorage is called', () => {
     wrapper.setState({
       incorrectFlashcards: [{ id: 1 }, { id: 2 }, { id: 3 }],
       category: 'Accessor'
@@ -47,29 +81,29 @@ describe('App', () => {
     expect(wrapper.state().category).toEqual('Welcome to Quizzy! Choose a category above.')
   });
 
-  it('should be able to add incorrect flashcards to state', () => {
-    let incorrectFlashcards = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    wrapper.setState({ incorrectFlashcards });
-    expect(wrapper.state().incorrectFlashcards.length).toEqual(3);
-    let newIncorrectFlashcards = { id: 4 };
-    wrapper.instance().saveToStorage(newIncorrectFlashcards);
-    let itemsInStorage = JSON.parse(localStorage.getItem('incorrectFlashcardsStorage')).length;
-    expect(itemsInStorage).toEqual(4);
-    // expect(wrapper.state().incorrectFlashcards).toEqual([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]);
-  })
 
   it('should update category in state based on passed value', () => {
     expect(wrapper.state().category).toEqual(null);
     wrapper.instance().updateCategory('Accessor');
     expect(wrapper.state().category).toEqual('Accessor');
+  });
+
+  it('should filter flashcards based on the current category', () => {
+    wrapper.setState({
+      category: 'Accessor',
+      flashcards: [
+        { type: 'accessor' },
+        { type: 'iteration' },
+        { type: 'accessor' },
+        { type: 'accessor' }
+      ]
+    });
+    expect(wrapper.state().category).toEqual('Accessor');
+    let filteredCards = wrapper.instance().filterCardsByCategory();
+    expect(filteredCards).toEqual([
+      { type: 'accessor' },
+      { type: 'accessor' },
+      { type: 'accessor' }
+    ]);
   })
-
-  
-
-  // it('should render the NavBar, PlayerControl, and FlashcardContainer components', () => {
-  //   wrapper.setState({ error: false })
-  //   expect(wrapper.find('Navbar').length).toEqual(1)
-  //   expect(wrapper.find('PlayerControl').length).toEqual(1)
-  //   expect(wrapper.find('FlashcardContainer').length).toEqual(1)
-  // })
 });
